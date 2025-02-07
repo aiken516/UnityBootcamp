57fe19339222d6bcbd1c755aab18ac2674f5036d
aiken516 <51198185+aiken516@users.noreply.github.com>
2025-02-07 17:46:01 +0900

    feat : Audio Mixer를 이용한 사운드 관리 시스템

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,32 +1,43 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSourceBGM;
-    [SerializeField] private TextMeshProUGUI _bgmButton;
+
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _masterVolumeSlider;
+    [SerializeField] private Slider _bgmVolumeSlider;
+    [SerializeField] private Slider _sfxVolumeSlider;
 
     private bool _isBGMPlaying;
 
     private void Start()
     {
         _audioSourceBGM.Play();
+
+        _masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+        _bgmVolumeSlider.onValueChanged.AddListener(SetBGMVolume);
+        _sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
-    public void OnBGMButtonClick()
+    private void SetMasterVolume(float volume)
     {
-        if (_isBGMPlaying)
-        {
-            _audioSourceBGM.Pause();
-            _bgmButton.text = "Off";
-        }
-        else 
-        {
-            _audioSourceBGM.UnPause();
-            _bgmButton.text = "On";
-        }
-
-        _isBGMPlaying = !_isBGMPlaying;
+        //슬라이더 최소~최대값은 -80~0
+        //따라서 수치 변경 시 Mathf.Log10 * 20을 이용해서 범위를 설정
+        //슬라이더 최소 값이 0.00001이어야 -80일 경우 0으로 계산(0이 되면 최대값으로 돌아감)
+        _audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
     }
 
+    private void SetBGMVolume(float volume)
+    {
+        _audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+
+    private void SetSFXVolume(float volume)
+    {
+        _audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
 }
